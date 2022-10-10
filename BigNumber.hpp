5b2acb47fc45f120371b28b5ef32ec4a3c2a2ae4
QinYuzhou <90183764+QinYuzhou@users.noreply.github.com>
2022-10-10 21:37:29 +0800

    Add files via upload

--- BigNumber.hpp
+++ BigNumber.hpp
@@ -100,6 +100,46 @@ struct Big_number
         num[0] = 0;
     }
 
+    Big_number(long long number) //将long long转化为Big_Number
+    {
+        if (number < 0)
+        {
+            number *= -1;
+            positive = false;
+        }
+        else
+        {
+            positive = true;
+        }
+        power = 0;
+        while (number && number % 10 == 0)
+        {
+            number /= 10;
+            power++;
+        }
+        length = 0;
+        long long x = number;
+        while (x) //计算位数
+        {
+            x /= 10;
+            length++;
+        }
+        if (!length)
+        {
+            num = new int[1];
+            length = 1;
+        }
+        else
+        {
+            num = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                num[i] = number % 10;
+                number /= 10;
+            }
+        }
+    }
+
     void clear() //将数字归零
     {
         positive = true;
@@ -108,6 +148,53 @@ struct Big_number
         delete[] num;
         num = new int[1];
         num[0] = 0;
+        return;
+    }
+
+    void fixed() //若数字的有效数字大于100位，则将其改为100位
+    {
+        if (length <= 100)
+            return;
+        if (num[length - 101] >= 5)
+        {
+            num[length - 100]++;
+            int now = length - 100;
+            while (now < length - 1 && num[now] == 10)
+            {
+                std::cout << now << " " << num[now] << std::endl;
+                num[now + 1] += num[now] / 10;
+                num[now] %= 10;
+                now++;
+            }
+            std::cout << length << " " << now << " " << num[now] << std::endl;
+            if (num[now] == 10)
+            {
+                power += length;
+                delete[] num;
+                num = new int[1];
+                length = 1;
+                num[0] = 1;
+                return;
+            }
+        }
+        std::cout << "www" << std::endl;
+        int new_len = 100;
+        while (new_len > 0 && num[length - new_len] == 0) //去除后导0
+        {
+            new_len--;
+        }
+        if (!new_len)
+        {
+            clear();
+            return;
+        }
+        int *digits = new int[new_len];
+        for (int i = 0; i < new_len; i++)
+        {
+            digits[i] = num[length - new_len + i];
+        }
+        power += length - new_len;
+        length = new_len;
         return;
     }
 
@@ -265,6 +352,7 @@ struct Big_number
                 len--;
             }
             result.length = len - result.power;
+            delete[] result.num;
             result.num = new int[result.length];
             for (int i = 0; i < result.length; i++)
             {
@@ -329,6 +417,7 @@ struct Big_number
                 len--;
             }
             result.length = len - result.power;
+            delete[] result.num;
             result.num = new int[result.length];
             for (int i = 0; i < result.length; i++)
             {
@@ -342,117 +431,8 @@ struct Big_number
 
     Big_number operator-(Big_number number) //进行减法计算,a-b就是a+(-b)
     {
-        Big_number result;
-        if (positive != number.positive) //符号相反,将数字位相加
-        {
-            result.positive = positive;
-            int *digits;                                                                                                       //存储加起来之后的数字位结果
-            int len;                                                                                                           // digits数组的长度
-            int min_digit = std::min(power, number.power), max_digit = std::max(length + power, number.length + number.power); //计算出加法后最低位和最高位的位置
-            len = max_digit - min_digit + 1;                                                                                   //计算出数位加法后的位数
-            digits = new int[len];
-            memset(digits, 0, len * sizeof(int)); //将digits清零
-            for (int i = 0; i < length; i++)
-            {
-                digits[power + i - min_digit] += num[i];
-            }
-            for (int i = 0; i < number.length; i++)
-            {
-                digits[number.power + i - min_digit] += number.num[i];
-            }
-            for (int i = 0; i < len - 1; i++)
-            {
-                digits[i + 1] += digits[i] / 10;
-                digits[i] %= 10;
-            }
-            result.power = 0;
-            while (result.power < len && digits[result.power] == 0) //去除后导0
-            {
-                result.power++;
-            }
-            if (result.power == len) // result为0
-            {
-                result.clear();
-                return result;
-            }
-            while (digits[len - 1] == 0) //去除前导0
-            {
-                len--;
-            }
-            result.length = len - result.power;
-            result.num = new int[result.length];
-            for (int i = 0; i < result.length; i++)
-            {
-                result.num[i] = digits[i + result.power];
-            }
-            result.power += min_digit;
-            delete[] digits;
-        }
-        else //符号位相同
-        {
-            int *digits;                                                                                                       //存储加起来之后的数字位结果
-            int len;                                                                                                           // digits数组的长度
-            int min_digit = std::min(power, number.power), max_digit = std::max(length + power, number.length + number.power); //计算出加法后最低位和最高位的位置
-            len = max_digit - min_digit + 1;                                                                                   //计算出数位加法后的位数
-            digits = new int[len];
-            memset(digits, 0, len * sizeof(int)); //将digits清零
-            for (int i = 0; i < length; i++)
-            {
-                digits[power + i - min_digit] += num[i] * (positive ? 1 : -1);
-            }
-            for (int i = 0; i < number.length; i++)
-            {
-                digits[number.power + i - min_digit] += number.num[i] * (number.positive ? -1 : 1);
-            }
-            for (int i = 0; i < len - 1; i++)
-            {
-                if (digits[i] < 0)
-                {
-                    digits[i] += 10;
-                    digits[i + 1]--;
-                }
-            }
-            result.power = 0;
-            while (result.power < len && digits[result.power] == 0) //去除后导0
-            {
-                result.power++;
-            }
-            if (result.power == len) // result为0
-            {
-                result.clear();
-                return result;
-            }
-            while (digits[len - 1] == 0) //去除前导0
-            {
-                len--;
-            }
-            if (digits[len - 1] < 0) //该数为负数,将每一位反转
-            {
-                for (int i = result.power; i < len; i++)
-                    digits[i] *= -1;
-                for (int i = 0; i < len - 1; i++)
-                {
-                    if (digits[i] < 0)
-                    {
-                        digits[i] += 10;
-                        digits[i + 1]--;
-                    }
-                }
-            }
-            while (digits[len - 1] == 0) //去除前导0
-            {
-                len--;
-            }
-            result.length = len - result.power;
-            result.num = new int[result.length];
-            for (int i = 0; i < result.length; i++)
-            {
-                result.num[i] = digits[i + result.power];
-            }
-            result.power += min_digit;
-            delete[] digits;
-        }
-        return result;
+        number.positive = !number.positive;
+        return (*this) + number;
     }
 
     Big_number operator*(Big_number number) //进行乘法计算
@@ -482,6 +462,7 @@ struct Big_number
             len--;
         }
         result.length = len - result.power;
+        delete[] result.num;
         result.num = new int[result.length];
         for (int i = 0; i < result.length; i++)
         {
@@ -491,13 +472,122 @@ struct Big_number
         result.power += power + number.power;
         return result;
     }
+
+    Big_number half() //返回该数除2的结果(除2就是乘0.5)
+    {
+        Big_number result;
+        result.positive = positive;
+        if (num[0] & 1)
+        {
+            result.power = power - 1;
+            if (num[length - 1] == 1)
+            {
+                delete[] result.num;
+                result.num = new int[length];
+                result.length = length;
+                for (int i = 1; i < length; i++)
+                {
+                    result.num[i] = (num[i] & 1) * 5;
+                    result.num[i] += num[i - 1] / 2;
+                }
+                result.num[0] = 5;
+            }
+            else //末尾5首位多1位
+            {
+                delete[] result.num;
+                result.num = new int[length + 1];
+                result.length = length + 1;
+                for (int i = 1; i <= length - 1; i++)
+                {
+                    result.num[i] = (num[i + 1] & 1) * 5;
+                    result.num[i] += num[i - 1] / 2;
+                }
+                result.num[0] = 5;
+                result.num[length] = num[length - 1] / 2;
+            }
+        }
+        else
+        {
+            result.power = power;
+            if (num[length - 1] == 1)
+            {
+                delete[] result.num;
+                result.num = new int[length - 1];
+                result.length = length - 1;
+                for (int i = 0; i < length - 1; i++)
+                {
+                    result.num[i] = (num[i + 1] & 1) * 5;
+                    result.num[i] += num[i] / 2;
+                }
+            }
+            else
+            {
+                delete[] result.num;
+                result.num = new int[length];
+                result.length = length;
+                for (int i = 0; i < length - 1; i++)
+                {
+                    result.num[i] = num[i] / 2;
+                    result.num[i] += (num[i + 1] & 1) * 5;
+                }
+                result.num[length - 1] = num[length - 1] / 2;
+            }
+        }
+        return result;
+    }
+
+    Big_number operator/(Big_number number) //进行除法计算 (a*10^k)/(b*10^m)=(a/b)*10^(k-m)
+    {
+        if (number.length == 1 && number.num[0] == 0)
+        {
+            return Big_number(0);
+        }
+        Big_number l, r;
+        int new_power = power - number.power;
+        number.power = power;
+        if (positive == number.positive) //结果为正数
+        {
+            l = Big_number(0), r = Big_number(1);
+            while (r * number < (*this))
+            {
+                l = r;
+                r = r * Big_number(2);
+            }
+        }
+        else //结果为负数
+        {
+            l = Big_number(-1), r = Big_number(0);
+            while ((*this) < l * number)
+            {
+                r = l;
+                l = l * Big_number(2);
+            }
+        }
+        int cnt = 0;
+        while (cnt < 1000)
+        {
+            Big_number mid = l + r;
+            mid = mid.half();
+            if ((*this) < (mid * number))
+            {
+                r = mid;
+            }
+            else
+            {
+                l = mid;
+            }
+            cnt++;
+        }
+        l.power += new_power;
+        return l;
+    }
 };
 
 std::ostream &operator<<(std::ostream &out, Big_number &num) //重载输出流
 {
     if (!num.positive) //如果是负数,则先输出负号
         out << "-";
-    if (num.power + num.length - 1 >= 20 || num.power + num.length - 1 <= -20) //该数的位数比20大时,使用科学计数法
+    if (num.length >= 20 || num.power + num.length >= 20 || num.power <= -20) //该数的位数比20大时,使用科学计数法
     {
         out << num.num[num.length - 1];
         if (num.length > 20)
@@ -516,8 +606,11 @@ std::ostream &operator<<(std::ostream &out, Big_number &num) //重载输出流
         {
             out << ".0";
         }
-        out << "e";
-        out << num.power + num.length - 1;
+        if (num.power + num.length - 1)
+        {
+            out << "e";
+            out << num.power + num.length - 1;
+        }
     }
     else if (num.length + num.power <= 0) //判断该数是否小于1
     {
